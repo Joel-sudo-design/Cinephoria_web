1ccbe541ebadb08c94c2e79060dfd52f4da9535c
Joel DERMONT <138232658+Joel-sudo-design@users.noreply.github.com>
2025-12-03 19:35:17 +0100

    Update 001-user.js

--- 001-user.js
+++ 001-user.js
@@ -1,6 +1,9 @@
 db = db.getSiblingDB(process.env.MONGODB_DB || 'cinephoria_mongo');
+
 db.createUser({
-    user: process.env.ME_USER || 'admin',
-    pwd:  process.env.ME_PASS || 'admin',
-    roles: [{ role: "readWrite", db: process.env.MONGODB_DB || 'cinephoria_mongo' }]
+  user: process.env.ME_USER || 'cinephoria_app',
+  pwd:  process.env.ME_PASS || 'motdepasse_app_bien_sale',
+  roles: [
+    { role: "readWrite", db: process.env.MONGODB_DB || 'cinephoria_mongo' }
+  ]
 });
